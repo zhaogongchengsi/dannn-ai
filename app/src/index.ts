9077d039ec29ef1dd8c49ec286e82e06a2066376
zhaogongchengsi <zzh1586169624@163.com>
2025-03-23 22:42:22 +0800

    添加配置状态管理，初始化配置状态以优化应用启动流程

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,10 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/index.css'
+import { useConfigState } from './composables/config.js'
 
 const app = createApp(App)
+const config = useConfigState()
 
 window.dannn.ipc.on('show', () => {
+	config.init()
 	app.mount('#app')
 })
 
