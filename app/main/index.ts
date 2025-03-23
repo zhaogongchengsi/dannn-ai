4966e2f93981213528cac92c7dc5583d454df66d
zhaogongchengsi <zzh1586169624@163.com>
2025-03-23 21:43:36 +0800

    移除 Hook 类的实例化，简化上下文管理，优化启动流程

--- app/main/index.ts
+++ app/main/index.ts
@@ -1,6 +1,4 @@
 import { app } from 'electron'
-import { withAsyncContext } from './lib/context'
-import { Hook } from './lib/hook'
 import { Window } from './lib/window'
 import { Loader } from './lib/loader'
 import { Config } from './lib/config'
@@ -12,7 +10,6 @@ if (!app.requestSingleInstanceLock()) {
 }
 
 const window = new Window()
-const hook = new Hook()
 const loader = new Loader()
 const config = new Config()
 
@@ -20,14 +17,7 @@ async function bootstrap() {
 	loader.init()
 	config.init()
 
-	withAsyncContext({
-		hook,
-		window,
-		loader,
-		config,
-	}, async () => {
-		await window.show()
-	})
+	await window.show()
 }
 
 bootstrap()
