9a6b2ce266d1e0c6221251398db95ea6d5475520
zhaogongchengsi <zzh1586169624@163.com>
2025-03-25 23:52:59 +0800

    重构侧边栏组件，新增菜单项及其图标，优化侧边栏内容结构

--- app/src/views/AppSidebar.vue
+++ app/src/views/AppSidebar.vue
@@ -5,16 +5,62 @@ import {
   SidebarContent,
   SidebarFooter,
   SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
   SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from '@/components/ui/sidebar'
 import SidebarTrigger from '@/components/ui/sidebar/SidebarTrigger.vue'
+import { Calendar, Home, Inbox, Search, Settings } from 'lucide-vue-next'
+// Menu items.
+const items = [
+  {
+    title: 'Home',
+    url: '#',
+    icon: Home,
+  },
+  {
+    title: 'Inbox',
+    url: '#',
+    icon: Inbox,
+  },
+  {
+    title: 'Calendar',
+    url: '#',
+    icon: Calendar,
+  },
+  {
+    title: 'Search',
+    url: '#',
+    icon: Search,
+  },
+  {
+    title: 'Settings',
+    url: '#',
+    icon: Settings,
+  },
+]
 </script>
 
 <template>
   <Sidebar collapsible="icon">
     <SidebarContent>
-      <SidebarGroup />
-      <SidebarGroup />
+      <SidebarGroup>
+        <SidebarGroupContent>
+          <SidebarMenu>
+            <SidebarMenuItem v-for="item in items" :key="item.title">
+              <SidebarMenuButton as-child>
+                <a :href="item.url">
+                  <component :is="item.icon" />
+                  <span>{{ item.title }}</span>
+                </a>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroupContent>
+      </SidebarGroup>
     </SidebarContent>
     <SidebarFooter>
       <div
