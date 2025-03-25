1d43da88d56b05929524a03a20c1bc6b8af2e15e
zhaogongchengsi <zzh1586169624@163.com>
2025-03-25 23:27:56 +0800

    新增侧边栏组件及其相关功能，优化应用结构，添加全局样式

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,7 +1,10 @@
 <script setup lang='ts'>
 import ModeToggle from './components/mode-toggle.vue'
+import SidebarProvider from './components/ui/sidebar/SidebarProvider.vue'
+import SidebarTrigger from './components/ui/sidebar/SidebarTrigger.vue'
 import { useConfig } from './composables/config'
 import { useExtension } from './composables/extension'
+import AppSidebar from './views/AppSidebar.vue'
 
 useConfig().init()
 useExtension().init()
@@ -9,6 +12,10 @@ useExtension().init()
 
 <template>
   <div class="w-screen h-screen">
-    <ModeToggle />
+    <SidebarProvider>
+      <AppSidebar />
+      <ModeToggle />
+      <SidebarTrigger />
+    </SidebarProvider>
   </div>
 </template>
---
+++ app/src/views/AppSidebar.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+} from '@/components/ui/sidebar'
+</script>
+
+<template>
+  <Sidebar>
+    <SidebarHeader>
+      as
+    </SidebarHeader>
+    <SidebarContent>
+      <SidebarGroup />
+      <SidebarGroup />
+    </SidebarContent>
+    <SidebarFooter>
+      asd
+    </SidebarFooter>
+  </Sidebar>
+</template>
