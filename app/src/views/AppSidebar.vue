853b8b31fe8ed57d74193245c160f6c5a72ed572
zhaogongchengsi <zzh1586169624@163.com>
2025-03-26 23:00:25 +0800

    新增侧边栏头部组件

--- app/src/views/AppSidebar.vue
+++ app/src/views/AppSidebar.vue
@@ -46,6 +46,9 @@ const items = [
 
 <template>
   <Sidebar collapsible="icon">
+	<SidebarHeader>
+		header
+	</SidebarHeader>
     <SidebarContent>
       <SidebarGroup>
         <SidebarGroupContent>
