61b35106b773a97f6f74144ce56f577f70f8e4fb
zhaozunhong <zzhhaon@163.com>
2025-03-28 14:23:46 +0800

    新增 Toggle 组件，优化 README 页面展示，调整主题处理逻辑

--- app/src/pages/readme.vue
+++ app/src/pages/readme.vue
@@ -1,4 +1,6 @@
 <script setup lang='ts'>
+import Button from '@/components/ui/button/Button.vue'
+import { Toggle } from '@/components/ui/toggle'
 import { useConfig } from '@/composables/config'
 import { useExtension } from '@/composables/extension'
 import { markdownToHtml } from '@/lib/shiki'
@@ -13,13 +15,14 @@ const config = useConfig()
 const name = route.query.name as string
 
 const extensionMeta = computed(() => extension.findExtension(name))
+const theme = computed(() => config.mode.value === 'dark' ? 'vitesse-dark' : 'vitesse-light')
 
 const readme = computedAsync(async () => {
   if (!extensionMeta.value || !extensionMeta.value.readme) {
     return ''
   }
 
-  const html = await markdownToHtml(extensionMeta.value.readme, config.mode.value === 'dark' ? 'vitesse-dark' : 'vitesse-light')
+  const html = await markdownToHtml(extensionMeta.value.readme, theme.value)
     .catch((err) => {
       console.error(err)
       return `Failed to load readme for ${name} ${err.message}`
@@ -32,12 +35,12 @@ const readme = computedAsync(async () => {
 <template>
   <div>
     <article v-if="extensionMeta">
-      <header class="p-4 flex items-center">
-        <div class="mr-4">
-          <img v-if="extensionMeta.icon" :src="extensionMeta.icon" alt="logo" class="w-16 h-16">
-          <img v-else src="/logo.png" alt="logo" class="w-16 h-16">
-        </div>
+      <header class="p-4 flex items-center gap-3 sticky top-0 bg-[hsl(var(--background))] z-10">
         <div>
+          <img v-if="extensionMeta.icon" :src="extensionMeta.icon" alt="logo" class="size-[100px]">
+          <img v-else src="/logo.png" alt="logo" class="size-[100px]">
+        </div>
+        <div class="flex-1">
           <h1 class="text-xl font-bold">
             {{ extensionMeta.name }}
           </h1>
@@ -50,9 +53,15 @@ const readme = computedAsync(async () => {
             {{ extensionMeta.description }}
           </p>
         </div>
+        <div class="flex items-center gap-2">
+          <Toggle>禁用</Toggle>
+          <Button>
+            卸载
+          </Button>
+        </div>
       </header>
       <div class="p-4">
-        <div class="prose" v-html="readme" />
+        <div class="prose dark:prose-invert w-full" v-html="readme" />
       </div>
     </article>
     <div v-else>
