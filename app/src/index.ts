25d2253f394698048fb33982808dd01e2fdbbd3e
zhaozunhong <zzhhaon@163.com>
2025-03-28 17:42:08 +0800

    新增 Pinia 状态管理库并集成到应用中，优化 Vite 配置以支持自动导入功能

--- app/src/index.ts
+++ app/src/index.ts
@@ -3,8 +3,12 @@ import App from './App.vue'
 import { router } from './router'
 import './assets/index.css'
 import { AIPlugin } from './plugin/ai'
+import { createPinia } from 'pinia'
 
+const pinia = createPinia()
 const app = createApp(App)
+
+app.use(pinia)
 app.use(router)
 app.use(AIPlugin())
 
--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,11 +4,28 @@ import vue from '@vitejs/plugin-vue'
 import tailwind from 'tailwindcss'
 import VueRouter from 'unplugin-vue-router/vite'
 import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [vue(), VueRouter({})],
+  plugins: [
+    vue(), 
+    VueRouter({}), 
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        '@vueuse/core'
+      ],
+      dirs: [
+        './src/components',
+        './src/composables',
+        './src/libs',
+        './src/utils',
+      ]
+    })
+  ],
   css: {
     postcss: {
       plugins: [tailwind()],
