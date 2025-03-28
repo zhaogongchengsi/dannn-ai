08b46340d1bacae5f6e1cb93bd94a8f2f9c8cdd9
zhaozunhong <zzhhaon@163.com>
2025-03-28 16:39:20 +0800

    新增 Textarea 组件并在聊天页面中集成，优化错误处理和加载状态显示

--- app/src/pages/chat/[name].vue
+++ app/src/pages/chat/[name].vue
@@ -5,6 +5,7 @@ import { AI } from '@/lib/ai';
 import { computedAsync } from '@vueuse/core';
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
+import Textarea from '@/components/textarea/Textarea.vue';
 
 const route = useRoute<'/chat/[name]'>()
 const extension = useExtension()
@@ -53,7 +54,14 @@ const aiAction = computedAsync(async () => {
 
 <template>
    <div class="size-full overflow-auto">
-      <div v-if="error" class="p-2 text-red-600"><pre><code>{{ error.message }}</code></pre></div>
-      <div v-else>{{ aiAction?.name }}</div>
+      <div v-if="error && !loading" class="p-2 text-red-600"><pre><code>{{ error.message }}</code></pre></div>
+      <div v-else-if="!aiAction && !loading">Ai 初始化失败 试试重启大法</div>
+      <div v-else-if="loading">loading...</div>
+      <div v-else class="w-full">
+         <div>message list</div>
+         <div>
+            <Textarea />
+         </div>
+      </div>
    </div>
 </template>
