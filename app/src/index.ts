3270d6b016dbda247909f4354e302033b90a8ab8
zhaozunhong <zzhhaon@163.com>
2025-03-28 09:51:16 +0800

    优化窗口准备逻辑，确保应用在窗口显示前完成挂载

--- app/src/index.ts
+++ app/src/index.ts
@@ -6,10 +6,19 @@ import './assets/index.css'
 const app = createApp(App)
 app.use(router)
 
+let ok = false
 window.dannn.ipc.on('window.show', () => {
   document.startViewTransition(() => {
     app.mount('#app')
+    ok = true
   })
 })
 
-window.dannn.ipc.send('window.ready')
+async function bootstrap() {
+  window.dannn.ipc.send('window.ready')
+  await new Promise(resolve => setTimeout(resolve, 100))
+  if (!ok) {
+    requestAnimationFrame(bootstrap)
+  }
+}
+requestAnimationFrame(bootstrap)
