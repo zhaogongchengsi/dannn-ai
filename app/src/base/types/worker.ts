74a3855f7a22a1f780d151784cdfb1685390c7e0
zhaogongchengsi <zzh1586169624@163.com>
2025-03-29 22:54:38 +0800

    优化工作线程消息处理，新增错误和结果消息类型，重构消息传递逻辑

--- app/src/base/types/worker.ts
+++ app/src/base/types/worker.ts
@@ -1,6 +1,37 @@
+export interface WorkerDoneMessage {
+  type: 'done'
+}
+
+export interface WorkerErrorMessage {
+  type: 'error'
+  error: string
+}
+
+export interface WorkerCallResultMessage {
+  type: 'call-result'
+  id: string
+  result: any
+}
+
+export interface WorkerCallErrorMessage {
+  type: 'call-error'
+  id: string
+  error: string
+}
+
+export interface WorkerModuleMessage {
+	type: 'module',
+	name: string
+}
+
+export type WorkerMessage = WorkerDoneMessage | WorkerErrorMessage | WorkerCallResultMessage | WorkerCallErrorMessage | WorkerModuleMessage
+
 export interface DnWorkerEvents {
   'error': Error
   'loaded': string
   'unloaded': void
   'status-changed': string
+  'worker:message': WorkerMessage
+  'worker:error': any
+  'worker:messageerror': any
 }
--- app/src/base/worker/worker.ts
+++ app/src/base/worker/worker.ts
@@ -1,4 +1,4 @@
-import type { DnWorkerEvents } from '../types/worker'
+import type { DnWorkerEvents, WorkerMessage } from '../types/worker'
 import { DnEvent } from '../common/event'
 
 export class DnWorker extends DnEvent<DnWorkerEvents> {
@@ -6,9 +6,10 @@ export class DnWorker extends DnEvent<DnWorkerEvents> {
   private isReady = false
   name: string
   url: string
+  promiserMap: Map<string, PromiseWithResolvers<any>> = new Map()
+  readyMethodsName: Set<string> = new Set()
 
   constructor(name: string, url: string) {
-    console.log('DnWorker', name, url)
     super()
     this.name = name
     this.url = url
@@ -16,12 +17,77 @@ export class DnWorker extends DnEvent<DnWorkerEvents> {
       type: 'module',
     })
     this.worker.onmessage = (e) => {
-      console.log('worker message', e)
+      this.emit('worker:message', e.data)
+      this.onMessage(e)
     }
 
     this.worker.onerror = (e) => {
-      console.error('worker error', e)
+      this.emit('worker:error', e)
     }
+
+    this.worker.onmessageerror = (e) => {
+      this.emit('worker:messageerror', e)
+    }
+  }
+
+  private onMessage(message: any) {
+    if (!('type' in message.data)) {
+      return
+    }
+    message = message as WorkerMessage
+    switch (message.data.type) {
+      case 'done':
+        this.isReady = true
+        this.emit('loaded', this.name)
+        break
+      case 'call-result':
+        const promiser = this.promiserMap.get(message.data.id)
+        if (promiser) {
+          promiser.resolve(message.data.result)
+        }
+        break
+      case 'call-error':
+        const promiserError = this.promiserMap.get(message.data.id)
+        if (promiserError) {
+          promiserError.reject(new Error(message.data.error))
+        }
+        break
+	case 'module':
+		this.readyMethodsName.add(message.data.name)
+		break
+    }
+  }
+
+  invoke<T>(name: string, ...args: any[]) {
+	if (!this.readyMethodsName.has(name)) {
+	  return Promise.reject(new Error(`Method ${name} not ready`))
+	}
+
+    const id = this.generateId()
+    if (!this.isReady) {
+      return Promise.reject(new Error('Worker not ready'))
+    }
+
+    const promiser = Promise.withResolvers<T>()
+
+    this.promiserMap.set(id, promiser)
+
+    this.postMessage({
+      type: 'call',
+      name,
+      args,
+      id,
+    })
+
+    promiser.promise.finally(() => {
+      this.promiserMap.delete(id)
+    })
+
+    return promiser.promise
+  }
+
+  generateId(): string {
+    return `${this.name}-${Date.now()}-${Math.random().toString(36)}`
   }
 
   get isWorkerReady() {
