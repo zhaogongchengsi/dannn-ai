58cd3ca402cfaa4b92e899aeaf30c6a13512daed
zhaozunhong <zzhhaon@163.com>
2025-04-01 11:20:06 +0800

    删除 logger 和 schema 模块，清理不再使用的代码

--- app/main/lib/schema.ts
+++
@@ -1,8 +0,0 @@
-import Ajv from 'ajv'
-import addFormats from 'ajv-formats'
-import schema from '../schema.json'
-
-const ajv = new Ajv()
-addFormats(ajv)
-
-export const validate = ajv.compile(schema)
