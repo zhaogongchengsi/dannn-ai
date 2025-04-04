5d6ca045b981620a76d5a5d37f4c73e568ca43f2
zhaogongchengsi <zzh1586169624@163.com>
2025-04-04 16:57:59 +0800

    添加多个 UI 组件及其相关文件，更新表单组件以支持 vee-validate，优化组件导入和结构

--- app/src/components/ui/textarea/Textarea.vue
+++ app/src/components/ui/textarea/Textarea.vue
@@ -2,7 +2,6 @@
 import type { HTMLAttributes } from 'vue'
 import { cn } from '@/lib/utils'
 import { useVModel } from '@vueuse/core'
-import { useTemplateRef } from 'vue'
 
 const props = defineProps<{
   class?: HTMLAttributes['class']
@@ -12,10 +11,7 @@ const props = defineProps<{
 
 const emits = defineEmits<{
   (e: 'update:modelValue', payload: string | number): void
-  (e: 'enter', payload: string): void
 }>()
-
-const textarea = useTemplateRef<HTMLTextAreaElement>('textarea')
 
 const modelValue = useVModel(props, 'modelValue', emits, {
   passive: true,
@@ -24,5 +20,5 @@ const modelValue = useVModel(props, 'modelValue', emits, {
 </script>
 
 <template>
-  <textarea ref="textarea" v-model="modelValue" :class="cn('flex min-h-20 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 resize-none', props.class)" />
+  <textarea v-model="modelValue" :class="cn('flex min-h-20 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50', props.class)" />
 </template>
