80fe0a31f91a54e5958a81cbd83ba8595637a9c8
zhaogongchengsi <zzh1586169624@163.com>
2025-04-05 14:56:49 +0800

    重构答案消息结构，添加完整答案和流式答案消息类型；更新相关类型定义以支持新的消息格式

--- packages/schemas/src/channel.ts
+++ packages/schemas/src/channel.ts
@@ -7,11 +7,24 @@ export const questionMessage = z.object({
 	aiReplier: z.string().array(),
 })
 
-export const answerMessage = z.object({
+export const completeAnswerMessage = z.object({
+	type: z.literal('content'),
 	questionId: z.string(),
 	content: z.string(),
 	aiReplier: z.string(),
 })
+
+
+export const streamAnswerMessage = z.object({
+	type: z.literal('stream'),
+	id: z.number(),
+	questionId: z.string(),
+	content: z.string(),
+	aiReplier: z.string(),
+	complete: z.boolean(),
+})
+
+const answerMessage = z.union([completeAnswerMessage, streamAnswerMessage])
 
 export type QuestionMessage = z.infer<typeof questionMessage>
 export type AnswerMessage = z.infer<typeof answerMessage>
