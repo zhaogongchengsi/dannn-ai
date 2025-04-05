94333710c1244330cd60499c17aab6c71b8b2774
zhaogongchengsi <zzh1586169624@163.com>
2025-04-05 14:23:00 +0800

    优化聊天组件的结构，调整代码顺序以提高可读性

--- app/src/pages/chat.vue
+++ app/src/pages/chat.vue
@@ -1,10 +1,10 @@
 <script setup lang='ts'>
+import { onFormWorkerChannel } from '@/base/rxjs/channel'
 import { useChatStore } from '@/stores/chat'
 import ChatFooterBody from '@/views/chat-footer-body.vue'
 import ChatFooterHeader from '@/views/chat-footer-header.vue'
 import { onUnmounted, watchEffect } from 'vue'
 import { useRoute } from 'vue-router'
-import { onFormWorkerChannel } from '@/base/rxjs/channel'
 
 const route = useRoute()
 const chatStore = useChatStore()
@@ -24,14 +24,16 @@ onUnmounted(() => unsubscribe())
 </script>
 
 <template>
-<div class="flex flex-col w-full" style="height: calc(100vh - var(--app-header-height))">
-  <div style="height: calc(100vh - var(--app-header-height) - var(--app-chat-footer-height) - 2px)"
-    class="overflow-y-auto">
-    聊天
+  <div class="flex flex-col w-full" style="height: calc(100vh - var(--app-header-height))">
+    <div
+      style="height: calc(100vh - var(--app-header-height) - var(--app-chat-footer-height) - 2px)"
+      class="overflow-y-auto"
+    >
+      聊天
+    </div>
+    <div class="bg-[hsl(var(--background))]" style="height: var(--app-chat-footer-height);">
+      <ChatFooterHeader />
+      <ChatFooterBody />
+    </div>
   </div>
-  <div class="bg-[hsl(var(--background))]" style="height: var(--app-chat-footer-height);">
-    <ChatFooterHeader />
-    <ChatFooterBody />
-  </div>
-</div>
 </template>
