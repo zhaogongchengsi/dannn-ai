1f736ce04a035e62a8261cf8740c841968a841ff
zhaogongchengsi <zzh1586169624@163.com>
2025-06-02 00:05:47 +0800

    feat: 添加 electron-builder 配置文件，更新构建脚本，优化扩展资源处理

--- src/node/main/constant.ts
+++ src/node/main/constant.ts
@@ -1,6 +1,8 @@
 import { join } from 'node:path'
+import process from 'node:process'
 import { app } from 'electron'
 
 export const APP_DATA_PATH = !app.isPackaged ? join(app.getAppPath(), '.dannn') : join(app.getPath('userData'), '.dannn')
-export const EXTENSIONS_ROOT = !app.isPackaged ? join(app.getAppPath(), 'extensions') : join(app.getPath('userData'), '.dannn', 'extensions')
+// export const EXTENSIONS_ROOT = !app.isPackaged ? join(app.getAppPath(), 'extensions') : join(app.getPath('userData'), '.dannn', 'extensions')
+export const EXTENSIONS_ROOT = !app.isPackaged ? join(app.getAppPath(), 'extensions') : join(process.resourcesPath, 'extensions')
 export const PROTOCOL_NAME = 'dannn'
